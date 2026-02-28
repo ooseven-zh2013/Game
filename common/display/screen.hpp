fe5d5d9ef9f37814e9d97f1b6a1def59c9a5229b
ooseven <ooseven_zh2013@163.com>
2026-02-28 19:14:37 +0800

    补全了构造函数

--- common/display/screen.hpp
+++ common/display/screen.hpp
@@ -1,6 +1,21 @@
-// TODO 实现屏幕类的构造函数
+#include "colorchar.hpp"
+#include <vector>
+
+// TODO 实现屏幕类
 class Screen {
 public:
+  using vc = std::vector<ColorChar>;
+  using vvc = std::vector<vc>;
+  Screen() = default;
+  ~Screen() = default;
+  Screen(size_t n_, size_t m_) : scr{n_, vc{m_}} {}
+  size_t size() { return x_size() * y_size(); }
+  vvc &data() { return scr; }
+  size_t x_size() { return scr.size(); }
+  size_t y_size() { return x_size() ? scr[0].size() : 0; }
+
 protected:
+  vvc scr;
+
 private:
 };
